0505f49f1572fa3aaced6f6d77767aebeef6efbe
bashar-the-coder <mat197194@gmail.com>
2022-12-01 17:49:26 +0600

    page routing added

---
+++ pages/newf/new_page_4.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import sys
+
+def hello():
+    st.write("hello world")
+
+x = 9
---
+++ pages/page_4.py
@@ -0,0 +1,8 @@
+import sys
+import streamlit as st
+from pages.newf.new_page_4 import *
+
+hello()
+
+st.write(x)
+
